4be5e467eba9e892f1473ff637e2c0a6d8e6950b
Jasmi <jasmine.shaheer@gmail.com>
2016-10-18 22:47:44 +0200

    Javadoc is generated

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,9 +1,22 @@
 import java.lang.Math.*;
+
 public class Conversor {
+	/**
+	 * 
+	 * @author Jasmi
+	 * Method to convert value from radians to degrees
+	 * @param value
+	 * @return double
+	 */
 	public double toDegrees(Double value){
 		return Math.toDegrees(value);
 		
 		}
+	/**
+	 * Method to convert value from degrees to radians
+	 * @param value
+	 * @return double
+	 */
 	public double toRadians(Double value){
 		return Math.toRadians(value);
 	}
